35455ea43427a709b6a232ae0937a871eea6344f
Guillermo Alvarado <guillermoalvarado89@gmail.com>
2018-09-13 13:15:13 -0500

    Suscribe popup

---
+++ static/js/myscript.js
@@ -0,0 +1,3 @@
+document.write('<script type="text/javascript" src="//downloads.mailchimp.com/js/signup-forms/popup/unique-methods/embed.js" data-dojo-config="usePlainJson: true, isDebug: false"></script>')
+
+window.dojoRequire(["mojo/signup-forms/Loader"], function(L) { L.start({"baseUrl":"mc.us19.list-manage.com","uuid":"7ca0350f86d9576b40bfe7cee","lid":"0649daac95","uniqueMethods":true}) })
